ac77b9194709d2b2e5ad68cc36790e7b279829e9
Tantawiiii <ahmedtramadan4@gmail.com>
2025-11-04 10:40:58 +0200

    readme

--- lib/features/home/cubit/search_cubit.dart
+++ lib/features/home/cubit/search_cubit.dart
@@ -33,3 +33,4 @@ class SearchCubit extends Cubit<SearchState> {
 }
 
 
+
--- lib/features/settings/cubit/update_profile_state.dart
+++ lib/features/settings/cubit/update_profile_state.dart
@@ -20,3 +20,4 @@ class UpdateProfileFailure extends UpdateProfileState {
   const UpdateProfileFailure(this.message);
 }
 
+
